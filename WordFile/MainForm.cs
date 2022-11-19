d8af93bd0839f0388ae22a78448caa581e36f220
wulex <827937686@qq.com>
2022-11-19 23:54:25 +0800

    update

--- WordFile/MainForm.cs
+++ WordFile/MainForm.cs
@@ -11,8 +11,8 @@ namespace WordFile
             InitializeComponent();
         }
 
-        private Word.Application G_WordApplication;//定义Word应用程序字段
-        private object G_Missing = Type.Missing;//定义G_Missing字段并添加引用
+        private Word.Application G_WordApplication; //定义Word应用程序字段
+        private object G_Missing = Type.Missing; //定义G_Missing字段并添加引用
 
         private void btnSelectFile_Click(object sender, EventArgs e)
         {
@@ -25,7 +25,6 @@ namespace WordFile
         }
 
 
-
         /// <summary>
         /// NPOI 组件调用的方法，只支持docx后缀
         /// </summary>
@@ -35,12 +34,12 @@ namespace WordFile
         {
             string docDirectory = txtDocDirectory.Text;
 
-            ThreadPool.QueueUserWorkItem(//开始线程池
-                (o) =>//使用Lambda表达式
+            ThreadPool.QueueUserWorkItem( //开始线程池
+                (o) => //使用Lambda表达式
                 {
                     DirectoryInfo dires = new DirectoryInfo(docDirectory);
                     FileInfo[] files = dires.GetFiles("*.docx");
-                
+
                     foreach (FileInfo f in files)
                     {
                         //文件路径
@@ -73,6 +72,7 @@ namespace WordFile
                                 {
                                     dic.Add(txtSearchKey4.Text, txtReplace4.Text);
                                 }
+
                                 if (txtSearchKey5.Text != "")
                                 {
                                     dic.Add(txtSearchKey5.Text, txtReplace5.Text);
@@ -86,20 +86,19 @@ namespace WordFile
                                     //ReplaceKeyword(para, dic);
                                 }
 
-                                using (var newstream = File.Create(f.Directory +"/"+ f.Name)) // "/poutput.docx"
+                                using (var newstream = File.Create(f.Directory + "/" + f.Name)) // "/poutput.docx"
                                 {
                                     doc.Write(newstream);
                                     newstream.Flush();
                                 }
-
-                                
                             }
                             catch (Exception ex)
                             {
-                                MessageBox.Show("工具异常:"+ ex.Message);
+                                MessageBox.Show("工具异常:" + ex.Message);
                             }
                         }
                     }
+
                     MessageBox.Show("替换完成");
                 });
         }
@@ -120,26 +119,26 @@ namespace WordFile
 
             string dir1 = txtDocDirectory.Text;
 
-            ThreadPool.QueueUserWorkItem(//开始线程池
-                (o) =>//使用Lambda表达式
+            ThreadPool.QueueUserWorkItem( //开始线程池
+                (o) => //使用Lambda表达式
                 {
                     DirectoryInfo dires = new DirectoryInfo(dir1);
                     FileInfo[] files = dires.GetFiles("*.doc");
-                    G_WordApplication =//创建Word应用程序对象
-                           new Word.Application();
+                    G_WordApplication = //创建Word应用程序对象
+                        new Word.Application();
                     foreach (FileInfo f in files)
                     {
-                        object P_FilePath = dir1 + "\\\\" + f;//创建Object对象
+                        object P_FilePath = dir1 + "\\\\" + f; //创建Object对象
 
                         try
                         {
-                            Word.Document P_Document = G_WordApplication.Documents.Open(//打开Word文档
-                             ref P_FilePath, ref G_Missing, ref G_Missing,
-                             ref G_Missing, ref G_Missing, ref G_Missing,
-                             ref G_Missing, ref G_Missing, ref G_Missing,
-                             ref G_Missing, ref G_Missing, ref G_Missing,
-                             ref G_Missing, ref G_Missing, ref G_Missing,
-                             ref G_Missing);
+                            Word.Document P_Document = G_WordApplication.Documents.Open( //打开Word文档
+                                ref P_FilePath, ref G_Missing, ref G_Missing,
+                                ref G_Missing, ref G_Missing, ref G_Missing,
+                                ref G_Missing, ref G_Missing, ref G_Missing,
+                                ref G_Missing, ref G_Missing, ref G_Missing,
+                                ref G_Missing, ref G_Missing, ref G_Missing,
+                                ref G_Missing);
 
                             Word.Range P_Range = //得到文档范围
                                 P_Document.Range(ref G_Missing, ref G_Missing);
@@ -148,14 +147,14 @@ namespace WordFile
 
                             if (txtSearchKey1.Text != "")
                             {
-                                this.Invoke(//在窗体线程中执行
-                                    (MethodInvoker)(() =>//使用Lambda表达式
+                                this.Invoke( //在窗体线程中执行
+                                    (MethodInvoker)(() => //使用Lambda表达式
                                     {
-                                        P_Find.Text = txtSearchKey1.Text;//设置查找的文本
-                                        P_Find.Replacement.Text = txtReplace1.Text;//设置替换的文本
+                                        P_Find.Text = txtSearchKey1.Text; //设置查找的文本
+                                        P_Find.Replacement.Text = txtReplace1.Text; //设置替换的文本
                                     }));
-                                object P_Replace = Word.WdReplace.wdReplaceAll;//定义替换方式对象
-                                bool P_bl = P_Find.Execute(//开始替换
+                                object P_Replace = Word.WdReplace.wdReplaceAll; //定义替换方式对象
+                                bool P_bl = P_Find.Execute( //开始替换
                                     ref G_Missing, ref G_Missing, ref G_Missing,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref P_Replace,
@@ -164,14 +163,14 @@ namespace WordFile
 
                             if (txtSearchKey2.Text != "")
                             {
-                                this.Invoke(//在窗体线程中执行
-                                    (MethodInvoker)(() =>//使用Lambda表达式
+                                this.Invoke( //在窗体线程中执行
+                                    (MethodInvoker)(() => //使用Lambda表达式
                                     {
-                                        P_Find.Text = txtSearchKey2.Text;//设置查找的文本
-                                        P_Find.Replacement.Text = txtReplace2.Text;//设置替换的文本
+                                        P_Find.Text = txtSearchKey2.Text; //设置查找的文本
+                                        P_Find.Replacement.Text = txtReplace2.Text; //设置替换的文本
                                     }));
-                                object P_Replace = Word.WdReplace.wdReplaceAll;//定义替换方式对象
-                                bool P_bl = P_Find.Execute(//开始替换
+                                object P_Replace = Word.WdReplace.wdReplaceAll; //定义替换方式对象
+                                bool P_bl = P_Find.Execute( //开始替换
                                     ref G_Missing, ref G_Missing, ref G_Missing,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref P_Replace,
@@ -180,14 +179,14 @@ namespace WordFile
 
                             if (txtSearchKey3.Text != "")
                             {
-                                this.Invoke(//在窗体线程中执行
-                                    (MethodInvoker)(() =>//使用Lambda表达式
+                                this.Invoke( //在窗体线程中执行
+                                    (MethodInvoker)(() => //使用Lambda表达式
                                     {
-                                        P_Find.Text = txtSearchKey3.Text;//设置查找的文本
-                                        P_Find.Replacement.Text = txtReplace3.Text;//设置替换的文本
+                                        P_Find.Text = txtSearchKey3.Text; //设置查找的文本
+                                        P_Find.Replacement.Text = txtReplace3.Text; //设置替换的文本
                                     }));
-                                object P_Replace = Word.WdReplace.wdReplaceAll;//定义替换方式对象
-                                bool P_bl = P_Find.Execute(//开始替换
+                                object P_Replace = Word.WdReplace.wdReplaceAll; //定义替换方式对象
+                                bool P_bl = P_Find.Execute( //开始替换
                                     ref G_Missing, ref G_Missing, ref G_Missing,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref P_Replace,
@@ -196,46 +195,48 @@ namespace WordFile
 
                             if (txtSearchKey4.Text != "")
                             {
-                                this.Invoke(//在窗体线程中执行
-                                    (MethodInvoker)(() =>//使用Lambda表达式
+                                this.Invoke( //在窗体线程中执行
+                                    (MethodInvoker)(() => //使用Lambda表达式
                                     {
-                                        P_Find.Text = txtSearchKey4.Text;//设置查找的文本
-                                        P_Find.Replacement.Text = txtReplace4.Text;//设置替换的文本
+                                        P_Find.Text = txtSearchKey4.Text; //设置查找的文本
+                                        P_Find.Replacement.Text = txtReplace4.Text; //设置替换的文本
                                     }));
-                                object P_Replace = Word.WdReplace.wdReplaceAll;//定义替换方式对象
-                                bool P_bl = P_Find.Execute(//开始替换
-                                    ref G_Missing, ref G_Missing, ref G_Missing,
-                                    ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing,
-                                    ref G_Missing, ref G_Missing, ref G_Missing, ref P_Replace,
-                                    ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing);
-                            }
-                            if (txtSearchKey5.Text != "")
-                            {
-                                this.Invoke(//在窗体线程中执行
-                                    (MethodInvoker)(() =>//使用Lambda表达式
-                                    {
-                                        P_Find.Text = txtSearchKey5.Text;//设置查找的文本
-                                        P_Find.Replacement.Text = txtReplace5.Text;//设置替换的文本
-                                    }));
-                                object P_Replace = Word.WdReplace.wdReplaceAll;//定义替换方式对象
-                                bool P_bl = P_Find.Execute(//开始替换
+                                object P_Replace = Word.WdReplace.wdReplaceAll; //定义替换方式对象
+                                bool P_bl = P_Find.Execute( //开始替换
                                     ref G_Missing, ref G_Missing, ref G_Missing,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref P_Replace,
                                     ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing);
                             }
 
-                            G_WordApplication.Documents.Save(//保存文档
+                            if (txtSearchKey5.Text != "")
+                            {
+                                this.Invoke( //在窗体线程中执行
+                                    (MethodInvoker)(() => //使用Lambda表达式
+                                    {
+                                        P_Find.Text = txtSearchKey5.Text; //设置查找的文本
+                                        P_Find.Replacement.Text = txtReplace5.Text; //设置替换的文本
+                                    }));
+                                object P_Replace = Word.WdReplace.wdReplaceAll; //定义替换方式对象
+                                bool P_bl = P_Find.Execute( //开始替换
+                                    ref G_Missing, ref G_Missing, ref G_Missing,
+                                    ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing,
+                                    ref G_Missing, ref G_Missing, ref G_Missing, ref P_Replace,
+                                    ref G_Missing, ref G_Missing, ref G_Missing, ref G_Missing);
+                            }
+
+                            G_WordApplication.Documents.Save( //保存文档
                                 ref G_Missing, ref G_Missing);
-                            ((Word._Document)P_Document).Close(//关闭文档
+                            ((Word._Document)P_Document).Close( //关闭文档
                                 ref G_Missing, ref G_Missing, ref G_Missing);
                         }
                         catch (Exception g)
                         {
                         }
                     }
-                    ((Word._Application)G_WordApplication).Quit(//退出Word应用程序
-                                ref G_Missing, ref G_Missing, ref G_Missing);
+
+                    ((Word._Application)G_WordApplication).Quit( //退出Word应用程序
+                        ref G_Missing, ref G_Missing, ref G_Missing);
                     MessageBox.Show("替换完成");
                 });
         }
@@ -255,18 +256,17 @@ namespace WordFile
                     para.ReplaceText(kv.Key, kv.Value);
                 }
             }
-            
         }
 
         private void ReplaceKeyword(XWPFParagraph para, IDictionary<string, string> redic)
         {
-            string text = string.Empty;//段落文本
+            string text = string.Empty; //段落文本
             string styleid = para.Style;
             var runs = para.Runs;
             for (int i = 0; i < runs.Count; i++)
             {
                 var run = runs[i];
-                text = run.ToString();  
+                text = run.ToString();
                 foreach (var kv in redic)
                 {
                     if (text.Contains(kv.Key))
@@ -274,10 +274,9 @@ namespace WordFile
                         text = text.Replace(kv.Key, kv.Value);
                     }
                 }
+
                 runs[i].SetText(text, 0);
             }
         }
-
-
     }
 }
